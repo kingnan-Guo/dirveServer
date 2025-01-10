3a99c690265a4f3e6e19fd562ac57b224adefe2c
kingnan-Guo <373734199@qq.com>
2025-01-10 12:53:42 +0800

    更新temp

---
+++ modules/DRIVETEMP/DRIVETEMP.cpp
@@ -0,0 +1,22 @@
+#include "DRIVETEMP.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int DRIVETEMP_MAIN(int arch, char* argv[]){
+    std::cout << "DRIVETEMP_MAIN" << std::endl;
+    return 0;
+};
---
+++ modules/DRIVETEMP/DRIVETEMP.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int DRIVETEMP_MAIN(int arch, char* argv[]);
