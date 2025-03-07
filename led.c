5a5c448b1eefcf0e8e8c094fab9b140b9389bebd
kingnan-Guo <373734199@qq.com>
2025-03-07 21:23:30 +0800

    ud

--- led.c
+++ led.c
@@ -5,10 +5,39 @@
 #include <linux/uaccess.h>
 #include <linux/cdev.h>
 
+
+// GPIO寄存器基地址（树莓派3B+）
+// #define BCM2711_GPIO_BASE   0x3F200000;// GPIO功能选择寄存器
+// #define GPFSEL0             (GPIO_BASE + 0x00)
+// #define GPFSEL1             (GPIO_BASE + 0x04)
+// #define GPFSEL2             (GPIO_BASE + 0x08)
+// #define GPFSEL3             (GPIO_BASE + 0x0C)
+
+// static volatile unsigned int *RCC
+
+
+// static volatile unsigned int *gpio_base = (volatile unsigned int *)BCM2711_GPIO_BASE;
+// static volatile unsigned int *gpio_base = (volatile unsigned int *)ioremap(BCM2711_GPIO_BASE, 0x1000);
+
+
+
+#define BCM2837_GPIO_BASE   0x3F200000  // GPIO寄存器基地址
+#define GPIO_REG_SIZE       (4 * 1024)  // 定义了 GPIO 寄存器块的大小，(4 * 1024) 是 4 KB，这个大小可以用来映射 GPIO 寄存器的内存区域
+#define GPFSEL1             0x04        // 定义了 GPIO 功能选择寄存器 1 的偏移地址，0x04。这个寄存器用于设置 GPIO 引脚的工作模式，比如输入、输出或备用功能。每个 GPFSEL 寄存器控制多个 GPIO 引脚的功能
+#define GPSET0              0x1C        // 定义了 GPIO 设置寄存器 0 的偏移地址，0x1C。这个寄存器用于设置 GPIO 引脚为高电平（输出高）。如果需要将某个 GPIO 引脚置为高电平，就需要向这个寄存器写入对应引脚的位
+#define GPCLR0              0x28        // 定义了 GPIO 清除寄存器 0 的偏移地址，0x28。这个寄存器用于将 GPIO 引脚设置为低电平（输出低）。如果需要将某个 GPIO 引脚清零，就需要向这个寄存器写入对应引脚的位
+
+
+
+
 static int major; // 主设备号
 #define DEVICE_NAME "my_led";
 
 static struct class *my_led_class;
+
+
+
+
 
 
 static int led_open(struct inode *node, struct file *file){
@@ -83,7 +112,7 @@ static void __exit led_exit(void){
     // 销毁类
     class_destroy(my_led_class);
     // 释放设备号
-    unregister_chrdev(major, "led");
+    unregister_chrdev(major, DEVICE_NAME);
 
     printk(KERN_INFO "my_led unregistered.\n");
 }
