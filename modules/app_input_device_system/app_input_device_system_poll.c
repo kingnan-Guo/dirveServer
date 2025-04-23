bd6587c647c26526310a20a11da48686ed06423e
kingnan-Guo <48483738+kingnan-Guo@users.noreply.github.com>
2025-04-23 05:46:05 +0000

    11

--- modules/app_input_device_system/app_input_device_system_poll.c
+++ modules/app_input_device_system/app_input_device_system_poll.c
@@ -5,3 +5,12 @@
 // void app_input_device_system_poll_main(void){
 
 // }
+
+
+void app_input_device_system_poll_init(int argc, char *argv[]){
+    
+}
+
+void app_input_device_system_poll_main(int argc, char *argv[]){
+    app_input_device_system_poll_init(argc, argv);
+}
