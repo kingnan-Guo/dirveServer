c79db92c08e5308e7b67e77d2f729133c70cccf2
kingnan-Guo <48483738+kingnan-Guo@users.noreply.github.com>
2025-05-01 16:38:28 +0000

    更新 pinctrl 子系统 demo 实现

--- dirverModules/pinctrl_system/virtual_controller_driver.c
+++ dirverModules/pinctrl_system/virtual_controller_driver.c
@@ -27,18 +27,34 @@ static struct pinctrl_dev *global_pinctrl_dev;
 static const struct pinctrl_pin_desc pins[] = {
     PINCTRL_PIN(0, "virtual_controller_driver_pin0"),
     PINCTRL_PIN(1, "virtual_controller_driver_pin1"),
-    PINCTRL_PIN(2, "virtual_controller_driver_pin2")
+    PINCTRL_PIN(2, "virtual_controller_driver_pin2"),
+    PINCTRL_PIN(3, "virtual_controller_driver_pin2")
 }
 
+
+static virtual_function_desc{
+    const char *func_name;// 功能名称
+    const char **groups;// 组名称
+    int num_groups;// 组数量
+}
 
 // 用来配置复用有哪些功能
-static const char * functions[] = {
-    "gpio",
-    "i2c",
-    "uart",
-    "spi",
+// static const char * functions[] = {
+//     "gpio",
+//     "i2c",
+//     "uart",
+//     "spi",
+// }
+
+static struct virtual_function_desc global_functions_desc[] = {
+    {"gpio", {"virtual_controller_driver_pin0", "virtual_controller_driver_pin1", "virtual_controller_driver_pin2"}, 3},
+    {"i2c", {"virtual_controller_driver_pin0", "virtual_controller_driver_pin1"}, 2},
+    {"uart", {"virtual_controller_driver_pin0", "virtual_controller_driver_pin1"}, 2},
 }
 
+
+
+static unsigned long global_configs[4];// 用来配置引脚的配置值
 
 
 
@@ -73,8 +89,51 @@ static void virtual_pin_dbg_show(struct pinctrl_dev *pctldev, struct seq_file *s
     seq_printf(s, "pin %s\n", dev_name(pctldev->dev));// 打印 设备名称
 }
 
-// virtual_dt_node_to_map
+// virtual_dt_node_to_map 开始处理 某个设备树的 节点
+
+/** 设备树， 从这设备树里获取信息
+
+virtual_pincontroller{
+    compatible = "kingnan,virtual_pincontroller";
+    i2cgrp {
+        functions = "i2c", "i2c";// 复用功能
+        group = "pin0", "pin1";// 引脚组
+        config = <0x11 0x22>;// 配置值, 会在 virtual_pinconf_get 中获得到 配置值 
+    }
+}
+
+
+
+在 pinctrl_map  中 
+struct pinctrl_map {
+	const char *dev_name;
+	const char *name;
+	enum pinctrl_map_type type;
+	const char *ctrl_dev_name;
+	union {
+		struct pinctrl_map_mux mux;//   引脚复用功能
+		struct pinctrl_map_configs configs;
+	} data;
+};
+
+
+要把 设备树中 的 pino0 设置为 i2c 功能， 
+还需要 另一个 pinctrl_map 来 表示 config 配置值
+一个 引脚 对应 两个 pinctrl_map
+
+
+*/
+
 static int virtual_dt_node_to_map(struct pinctrl_dev *pctldev, struct device_node *np, struct pinctrl_map **map, unsigned long *nmaps){
+    //1、 确定 引脚个数 ， 分配 pinctrl_map 结构体
+    
+    //2、 逐个 取出 引脚 引脚功能 引脚个数
+
+
+    //3、 存入 pinctrl_map 结构体中
+    
+    
+    
     return -EINVAL;
 }
 
@@ -96,28 +155,42 @@ static const struct pinctrl_ops virtual_pctrl_ops = {
 
 
 
+// ----------------------
+
 
 /** 复用函数  */
 
 // virtual_pmx_get_funcs_count
 static int virtual_pmx_get_funcs_count(struct pinctrl_dev *pctldev){
-    return ARRAY_SIZE(functions);// 获取复用函数数量
+    // return ARRAY_SIZE(functions);// 获取复用函数数量
+    return ARRAY_SIZE(global_functions_desc);// 获取复用函数数量
 }
 
 // virtual_pmx_get_func_name
 static const char *virtual_pmx_get_func_name(struct pinctrl_dev *pctldev, unsigned selector){
-    return functions[selector];// 获取复用函数名称
+    return global_functions_desc[selector];// 获取复用函数名称
 }
 
 
 // virtual_pmx_get_groups
+// 获得 某一个 功能里面 组的信息
 static int virtual_pmx_get_groups(struct pinctrl_dev *pctldev, unsigned selector, const char **groups, unsigned *num_groups){
     
 
-    // *groups = 
-    // num_groups = 1;
+    *groups = global_functions_desc[selector].groups;// 获取 组名称
+    num_groups = global_functions_desc[selector].num_groups;// 获取组数 大小
+    return 0;
 }
 
+// virtual_pmx_set 设置 复用函数
+static int virtual_pmx_set(struct pinctrl_dev *pctldev, unsigned selector, unsigned group){
+    // 这里设置 复用函数
+    // 把 pctldev->desc->pins[group].name 这组 引脚 设置为 global_functions_desc[selector].func_name
+    printk("set %s as %s\n", pctldev->desc->pins[group].name, global_functions_desc[selector].func_name);
+
+    // 把 group 某一组 selector 引脚 配置为 某个功能
+    return 0;
+}
 
 
 // virtual_pmx_get_funcs_count 复用 引脚 配置
@@ -125,9 +198,60 @@ static const struct pinmux_ops virtual_pmx_ops = {
 	.get_functions_count = virtual_pmx_get_funcs_count,// 获取复用函数数量, 知道 支持 多少个 功能
 	.get_function_name = virtual_pmx_get_func_name,// 获取复用函数名称， 取出 每一个 功能的 名字
 	.get_function_groups = virtual_pmx_get_groups,// 获取复用函数组， 每个都是独立一组
-	.set_mux = virtual_pmx_set,// 设置复用函数
+	.set_mux = virtual_pmx_set,// 设置复用函数； 把某一组引脚 配置为 某个功能
 };
 
+
+
+
+
+// ---------------------    
+
+
+
+// virtual_pinconf_get 取出 某个 引脚的配置值 放入到 
+static int virtual_pinconf_get(struct pinctrl_dev *pctldev, unsigned pin_id, unsigned long *config){
+    *config = global_configs[pin_id];// 让 *config 配置值 等于 global_configs[pin_id] 里面的值； pin_id 是 引脚的索引
+    return 0;
+}
+
+// virtual_pinconf_set 设置 引脚的 配置值
+static int virtual_pinconf_set(struct pinctrl_dev *pctldev, unsigned pin_id, unsigned long *configs, unsigned num_configs){
+    if (num_configs != 1)// 如果配置数量不等于 1
+        return -EINVAL;// 返回 错误
+
+    global_configs[pin_id] = *configs;//  根据引脚的 id ： pin_id 设置 引脚的配置值
+
+
+    // 把 pin_id 这个引脚 配置为 *configs
+    printk("config %s as %s\n", pctldev->desc->pins[pin_id].name, global_configs[pin_id]);
+
+    // 因为没有 实际的 寄存器 配置 
+    
+    return 0;
+}
+
+
+
+// virtual_pinconf_dbg_show 打印 引脚的配置  信息
+static void virtual_pinconf_dbg_show(struct pinctrl_dev *pctldev, struct seq_file *s, unsigned pin_id){
+    seq_printf(s, "0x%lx", global_configs[pin_id]);// 打印 引脚的配置值
+}
+
+
+// virtual_pinconf_group_dbg_show 打印 引脚组 的配置 信息， 现在 一组 引脚 中只有 一个 引脚
+static void virtual_pinconf_group_dbg_show(struct pinctrl_dev *pctldev, struct seq_file *s, unsigned pin_id){
+    seq_printf(s, "0x%lx", global_configs[pin_id]);// 打印 引脚组的配置值
+}
+
+
+// pinconf_ops 引脚配置
+static const struct pinconf_ops virtual_pinconf_ops = {
+	.pin_config_get = virtual_pinconf_get,// 获取 引脚的配置值
+	.pin_config_set = virtual_pinconf_set,// 设置 引脚的配置值
+	.pin_config_dbg_show = virtual_pinconf_dbg_show,// 打印 引脚的配置值
+	.pin_config_group_dbg_show = virtual_pinconf_group_dbg_show,// 打印 引脚组的配置值
+};
 
 
 
@@ -154,6 +278,13 @@ static int virtual_controller_driver_probe(struct platform_device *pdev){
 
     // 设置引脚的 复用函数 pin mux
     pictrl->pmxops = &virtual_pmx_ops;// 复用函数
+
+
+    // 对引脚的配置  pin config
+
+    pictrl->confops = &virtual_pinconf_ops;// 引脚配置操作函数
+
+
 
 
 
