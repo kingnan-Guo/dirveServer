5454e7ecf8b05f5b2da8d2b129edb185363e36dc
kingnan-Guo <48483738+kingnan-Guo@users.noreply.github.com>
2025-05-27 12:14:26 +0000

    2025/05/27 00:10 # 层级中断     分支         dirver_raspberry_XXXXX_vX.X.X
        文件
            ./dirverModules/interrupt_system_level/virtual_interrupt_system.c
            ./modules/interrupt_system/gpio_key_drv.c

--- Makefile
+++ Makefile
@@ -171,13 +171,17 @@ ASM_SOURCES := $(wildcard $(APP_MODULES_DIR)/*/*.S)
 
 
 # # interrupt_system    子系统 ---------------------
-virtual_interrupt_system-y := $(MODULES_DIR)/interrupt_system/virtual_interrupt_system.o
+# virtual_interrupt_system-y := $(MODULES_DIR)/interrupt_system/virtual_interrupt_system.o
+# obj-m += virtual_interrupt_system.o
+# gpio_key_drv-y := $(MODULES_DIR)/interrupt_system/gpio_key_drv.o
+# obj-m += gpio_key_drv.o
+
+
+# # interrupt_system    子系统  层级---------------------
+virtual_interrupt_system-y := $(MODULES_DIR)/interrupt_system_level/virtual_interrupt_system.o
 obj-m += virtual_interrupt_system.o
 gpio_key_drv-y := $(MODULES_DIR)/interrupt_system/gpio_key_drv.o
 obj-m += gpio_key_drv.o
-
-
-
 
 
 all:
--- dirverModules/interrupt_system_level/virtual_interrupt_system.c
+++ dirverModules/interrupt_system_level/virtual_interrupt_system.c
@@ -16,62 +16,46 @@
 /* FIXME: for gpio_get_value() replace this with direct register read */
 #include <linux/gpio.h>
 #include <linux/of.h>
+#include <linux/of_irq.h>
 #include <linux/of_device.h>
 #include <linux/bug.h>
-#include <linux/random.h>
 
 static struct irq_domain *virtual_interrupt_domain;
 
-
-
-static void virtual_interrupt_irq_ack(struct irq_data *data)
-{
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-}
+static u32 upper_hwirq_base;
 
 static void virtual_interrupt_irq_mask(struct irq_data *data)
 {
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-}
-
-static void virtual_interrupt_irq_mask_ack(struct irq_data *data)
-{
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    irq_chip_mask_parent(data); // 屏蔽 父节点 中断 
 }
 
 static void virtual_interrupt_irq_unmask(struct irq_data *data)
 {
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    irq_chip_unmask_parent(data); // 解除 父节点 中断 屏蔽
+    // 这里可以添加一些其他的操作，比如设置寄存器等
+    // 解除屏蔽后，父节点的中断控制器会开始处理这个中断
 }
 
 static void virtual_interrupt_irq_eoi(struct irq_data *data)
 {
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    irq_chip_eoi_parent(data);
 }
 
-
 static struct irq_chip virtual_interrupt_irq_chip = {
-	.name			= "virtual_irq_controller",
-	.irq_ack	   = virtual_interrupt_irq_ack	   ,
-	.irq_mask	   = virtual_interrupt_irq_mask	   ,
-	.irq_mask_ack  = virtual_interrupt_irq_mask_ack ,
-	.irq_unmask    = virtual_interrupt_irq_unmask   ,
-	.irq_eoi	   = virtual_interrupt_irq_eoi	   ,
+    .name       = "virtual_irq_controller",
+    .irq_mask   = virtual_interrupt_irq_mask,
+    .irq_unmask = virtual_interrupt_irq_unmask,
+    .irq_eoi    = virtual_interrupt_irq_eoi,
 };
-
-
-
-
-
-
-
 
 /**
  * virtual_interrupt_domain_alloc
  * 要为 irq_desc 提供 handleA irq_data irq_chip
  * 
  */
-
 static int virtual_interrupt_domain_alloc(struct irq_domain *domain, unsigned int irq, unsigned int nr_irqs, void *data)
 {
     printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
@@ -86,13 +70,11 @@ static int virtual_interrupt_domain_alloc(struct irq_domain *domain, unsigned in
     hwirq = fwspec->param[0]; // 硬件中断号
 
     // nr_irqs 是 中断号的数量
-    for(int i = 0; i < nr_irqs; i++){
+    for (int i = 0; i < nr_irqs; i++) {
         // 设置 硬件中断号 和 chip
         // 这里就是 设置irq_desc[irq] 的 irq_data
         irq_domain_set_hwirq_and_chip(domain, irq + i, hwirq + i, &virtual_interrupt_irq_chip, NULL);
     }
-
-
 
     /** 2 设置 irq_desc[irq].hander_irq , 可能来自 GIC  */
     // 要看 intc 需要几个参数
@@ -108,32 +90,28 @@ static int virtual_interrupt_domain_alloc(struct irq_domain *domain, unsigned in
     parent_fwspec.param[1] = fwspec->param[1];// 0
     
     return irq_domain_alloc_irqs_parent(domain, irq, nr_irqs, &parent_fwspec); // 分配中断号
-
 }
 
-
-
-// virtual_interrupt_domain_translate
 /**
  * 解析 gpio 使用 虚拟中断控制器中的 中断号 和 
  * 
  */
-static int virtual_interrupt_domain_translate(struct irq_domain *d, struct irq_fwspec *fwspec, unsigned int *hwirq, unsigned int *type){
+static int virtual_interrupt_domain_translate(struct irq_domain *d, struct irq_fwspec *fwspec, unsigned long *hwirq, unsigned int *type)
+{
     printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
 
-    if(is_of_node(fwspec->node)){
-        if(fwspec->param_count != 2){
+    if (is_of_node(fwspec->fwnode)) {
+        if (fwspec->param_count != 2) {
             return -EINVAL;
         }
 
-        *hwirq = fwspec->param[0];// 硬件中断号
-        *type = fwspec->param[1];// 中断类型
-
+        *hwirq = fwspec->param[0]; // 硬件中断号
+        *type = fwspec->param[1]; // 中断类型
+        return 0;
     }
 
     return -EINVAL;
 }
-
 
 /**
  * 1 解析 translate
@@ -145,27 +123,25 @@ static int virtual_interrupt_domain_translate(struct irq_domain *d, struct irq_f
  *              }
  * 
  */
-
-
 static const struct irq_domain_ops virtual_interrupt_domain_ops = {
     .translate = virtual_interrupt_domain_translate, // 设备树节点 和 中断号 转换
     .alloc = virtual_interrupt_domain_alloc, // 分配中断号
     // .free = virtual_interrupt_domain_free, // 释放中断号
 };
 
-
-
 static int virtual_interrupt_probe(struct platform_device *pdev)
 {
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
 
-    struct device_node *np = pdev->dev.of_node;// 获得 设备树的节点
-    int irq_to_parent;//中断号
-    int irq_base; //虚拟中断号
-
+    struct device_node *np = pdev->dev.of_node; // 获得 设备树的节点
     struct irq_domain *parent_domain; // 父域
     struct device_node *parent; // 父设备树节点
 
+    // 从设备树 中 获取 static u32 upper_hwirq_base;
+    if (of_property_read_u32(np, "upper_hwirq_base", &upper_hwirq_base)) {
+        printk(KERN_ERR "Failed to read upper_hwirq_base from device tree\n");
+        return -EINVAL;
+    }
 
     /** 
      * 核心： 分配 / 设置 / 注册  irq_domain
@@ -177,13 +153,18 @@ static int virtual_interrupt_probe(struct platform_device *pdev)
      * 
      * 对于每一个 硬件中断号， 都要在  内核 有 irq_domain 里注册一个 irq_desc
      * */
-
     // 为节点 找到 父亲； 根据 设备树 中的 interrupt-parent 找到父亲节点
-    parent = of_irq_find_parent(np);// 获得父中断号
+    parent = of_irq_find_parent(np); // 获得父中断号
+    if (!parent) {
+        printk(KERN_ERR "Failed to find parent interrupt node\n");
+        return -ENODEV;
+    }
 
-
-    parent_domain = irq_find_host(parent);// 查找父域， 也就是查找设备树的节点对应的 irq_domain
-
+    parent_domain = irq_find_host(parent); // 查找父域， 也就是查找设备树的节点对应的 irq_domain
+    if (!parent_domain) {
+        printk(KERN_ERR "Failed to find parent IRQ domain\n");
+        return -ENODEV;
+    }
 
     //  static inline struct irq_domain *irq_domain_add_hierarchy(struct irq_domain *parent,
     //     unsigned int flags,
@@ -191,22 +172,24 @@ static int virtual_interrupt_probe(struct platform_device *pdev)
     //     struct device_node *node,
     //     const struct irq_domain_ops *ops,
     //     void *host_data)     
+    virtual_interrupt_domain = irq_domain_add_hierarchy(parent_domain, 0, 4, np, &virtual_interrupt_domain_ops, NULL); // 32 是虚拟中断号的范围
+    if (!virtual_interrupt_domain) {
+        printk(KERN_ERR "Failed to add IRQ domain\n");
+        return -ENOMEM;
+    }
 
-     virtual_interrupt_domain = irq_domain_add_hierarchy(parent_domain, 0, 4, np, &virtual_interrupt_domain_ops, NULL);// 32 是虚拟中断号的范围
-
-
-
-	return 0;
+    return 0;
 }
-
 
 static int virtual_interrupt_remove(struct platform_device *pdev)
 {
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-	return 0;
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    if (virtual_interrupt_domain) {
+        irq_domain_remove(virtual_interrupt_domain);
+        virtual_interrupt_domain = NULL;
+    }
+    return 0;
 }
-
-
 
 static const struct of_device_id virtual_interrupt_of_match[] = {
     { .compatible = "kingnan,virtual_irq_controller", },
@@ -214,32 +197,31 @@ static const struct of_device_id virtual_interrupt_of_match[] = {
 };
 
 static struct platform_driver virtual_interrupt_driver = {
-	.probe		= virtual_interrupt_probe,
-	.remove		= virtual_interrupt_remove,
-	.driver		= {
-		.name	= "virtual_irq_controller",
-		.of_match_table = of_match_ptr(virtual_interrupt_of_match),
-	}
+    .probe      = virtual_interrupt_probe,
+    .remove     = virtual_interrupt_remove,
+    .driver     = {
+        .name   = "virtual_irq_controller",
+        .of_match_table = of_match_ptr(virtual_interrupt_of_match),
+    }
 };
 
 /* 1. 入口函数 */
 static int __init virtual_interrupt_init(void)
-{	
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-	/* 1.1 注册一个platform_driver */
-	return platform_driver_register(&virtual_interrupt_driver);
+{   
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    /* 1.1 注册一个platform_driver */
+    return platform_driver_register(&virtual_interrupt_driver);
 }
 
 /* 2. 出口函数 */
 static void __exit virtual_interrupt_exit(void)
 {
-	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-	/* 2.1 反注册platform_driver */
-	platform_driver_unregister(&virtual_interrupt_driver);
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    /* 2.1 反注册platform_driver */
+    platform_driver_unregister(&virtual_interrupt_driver);
 }
 
 module_init(virtual_interrupt_init);
 module_exit(virtual_interrupt_exit);
 
 MODULE_LICENSE("GPL");
-
