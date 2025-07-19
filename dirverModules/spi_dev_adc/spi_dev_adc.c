a5d01109e809ab22380a05e8f334634395bd9ce7
kingnan-Guo <48483738+kingnan-Guo@users.noreply.github.com>
2025-07-19 08:54:05 +0000

    spi dac

--- dirverModules/spi_dev_adc/spi_dev_adc.c
+++ dirverModules/spi_dev_adc/spi_dev_adc.c
@@ -1,12 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Simple synchronous userspace interface to SPI devices
+ *
+ * Copyright (C) 2006 SWAPP
+ *	Andrea Paterniani <a.paterniani@swapp-eng.it>
+ * Copyright (C) 2007 David Brownell (simplification, cleanup)
+ */
+
 #include <linux/init.h>
-#include <linux/module.h>
 #include <linux/ioctl.h>
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/list.h>
 #include <linux/errno.h>
+#include <linux/mod_devicetable.h>
+#include <linux/module.h>
 #include <linux/mutex.h>
+#include <linux/property.h>
 #include <linux/slab.h>
 #include <linux/compat.h>
 #include <linux/of.h>
@@ -19,219 +30,144 @@
 #include <linux/uaccess.h>
 
 
-#define DEVICE_NAME "my_test_device"
-static int major; // 设备主号
-static int minor; // 次设备号
-static unsigned char my_buffer[1024]; // 内核空间缓冲区
+static struct spi_device *dac_spi;
+static int major;
 
-static struct class *my_test_device_class;
 
-static struct cdev my_test_device_cdev;
+static long
+spidev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	int			retval = 0;
+	struct spidev_data	*spidev;
+	struct spi_device	*spi;
 
-static dev_t dev;
-
-static int my_test_device_open(struct inode *inode, struct file *file) {
-    printk(KERN_INFO "%s %s %d \n", __FILE__, __FUNCTION__, __LINE__);
-    return 0;
+	return retval;
 }
 
-static ssize_t my_test_device_read(struct file *file, char __user *buffer, size_t len, loff_t *offset) {
-    printk(KERN_INFO "%s %s %d \n", __FILE__, __FUNCTION__, __LINE__);
-    // 读取数据到用户空间
-    // int copy_to_user(void __user *dst, const void *src, unsigned size)
-    // unsigned long len = len > 100 ? 100 : len;
-    // copy_to_user(buffer, my_buffer, len);
-
-    // 检查返回值
-    if (copy_to_user(buffer, my_buffer, len)) {
-        printk(KERN_WARNING "Failed to copy data to user space.\n");
-        return -EFAULT;  // 返回错误码
-    }
 
 
-    return len;  // 返回读取的字节数
+
+static int spidev_release(struct inode *inode, struct file *filp)
+{
+	struct spidev_data	*spidev;
+	return 0;
 }
 
-static ssize_t my_test_device_write(struct file *file, const char __user *buffer, size_t len, loff_t *offset) {
-    printk(KERN_INFO "%s %s %d \n", __FILE__, __FUNCTION__, __LINE__);
-
-    if (len > sizeof(my_buffer)) {
-        len = sizeof(my_buffer);  // 限制写入长度
-    }
-
-    // 从用户空间读取数据到内核空间
-    // copy_from_user(my_buffer, buffer, len);
-    // 检查返回值
-    if (copy_from_user(my_buffer, buffer, len)) {
-        printk(KERN_WARNING "Failed to copy data from user space.\n");
-        return -EFAULT;  // 返回错误码
-    }
-
-
-    return len;  // 返回写入的数据字节数
-}
-
-static int my_test_device_release(struct inode *inode, struct file *file) {
-    printk(KERN_INFO "%s %s %d \n", __FILE__, __FUNCTION__, __LINE__);
-    return 0;
-}
-
-static struct file_operations my_test_device_fops = {
-    .owner = THIS_MODULE,
-    .read = my_test_device_read,
-    .write = my_test_device_write,
-    .open = my_test_device_open,
-    .release = my_test_device_release
+static const struct file_operations spidev_fops = {
+	.owner =	THIS_MODULE,
+	/* REVISIT switch to aio primitives, so that userspace
+	 * gets more complete API coverage.  It'll simplify things
+	 * too, except for the locking.
+	 */
+	// .write =	spidev_write,
+	// .read =		spidev_read,
+	.unlocked_ioctl = spidev_ioctl,
+	// .compat_ioctl = spidev_compat_ioctl, // 这个 是 32 位程序调用 64 位的 ioctl
+	.release =	spidev_release,
 };
 
-// 模块加载时执行的函数
-static int __init my_test_device_init(void) {
-    int ret;
-    dev_t dev;
-    char device_name_buf[30];
+/*-------------------------------------------------------------------------*/
 
-    /**
-     * 1. 分配设备号
-     * 2. 注册设备号
-     * 3. 创建设备类
-     * 4. 创建设备节点
-     * 5. 注册设备驱动
-     * 
-    */
+/* The main reason to have this class is to make mdev/udev create the
+ * /dev/spidevB.C character device nodes exposing our userspace API.
+ * It also simplifies memory management.
+ */
 
-   /**
-    * 申请一个 主次设备号的空间
-    * alloc_chrdev_region
-    * params:
-    *      1. dev_t *: 设备号
-    *      2. unsigned int: 从哪个主设备号开始分配
-    *      3. unsigned int: 分配多少个设备号
-    *      4. const char *: 设备名称
-    * 
-    *  返回值: 0 成功，负数失败
-    * 
-   */
-
-    ret = alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME);
-    if(ret < 0){
-        printk(KERN_ALERT "alloc_chrdev_region failed \n");
-        return -EINVAL;
-    }
-    // 通过 MAJOR 宏从 devt 中提取出主设备号
-    major = MAJOR(dev);
-    // 通过 MINOR 宏从 devt 中提取出次设备号
-    minor = MINOR(dev);
+static struct class *spidev_class;
 
 
 
-    //初始化 cdev ，让 cdev 跟 file_operations 关联起来
-    // void cdev_init(struct cdev *, const struct file_operations *);
-    cdev_init(&my_test_device_cdev, &my_test_device_fops);
-
-    // 添加 cdev 到内核
-    // int cdev_add(struct cdev *, dev_t, unsigned);
-    // unsigned 占用几个 设备号
-    ret = cdev_add(&my_test_device_cdev, dev, 2);
-
-    if(ret < 0){
-        printk(KERN_ALERT "cdev_add failed \n");
-		return -EINVAL;
-    }
-
-    // class_create
-    // 创建设备类
-    // struct class *class_create(struct module *owner, const char *name);
+static const struct of_device_id spidev_dt_ids[] = {
+	{ .compatible = "spi_dev_dac" },
+	{},
+};
+MODULE_DEVICE_TABLE(of, spidev_dt_ids);
 
 
 
-    snprintf(device_name_buf, sizeof(device_name_buf), "%s_%s", DEVICE_NAME, "_class");
-    my_test_device_class = class_create(device_name_buf);
-    if(IS_ERR(my_test_device_class)){
-        // pr_err("failed to allocate class\n");
-        printk("failed to allocate class\n");
-        return PTR_ERR(my_test_device_class);
-    }
 
-    /**
-     * 创建设备节点
-     *  params:
-     *      1. class: 设备类
-     *      2. dev_t: 设备号
-     *      3. const char *: 设备节点名称
-     *      4. dev_t: 设备号            MKDEV(major, 0) 是个整数
-     *      5. const char *: 设备节点名称
-     *    *      6. void *: 私有数据
-     * 
-     *  会去 创建一个 设备节点 /dev/my_test_device
-     */
-    // device_create(my_test_device_class, NULL, dev, NULL, DEVICE_NAME);
+/*-------------------------------------------------------------------------*/
 
-    // char device_name_buf[30];
-    snprintf(device_name_buf, sizeof(device_name_buf), "%s_%d", DEVICE_NAME, minor);
-    device_create(my_test_device_class, NULL, MKDEV(major, minor), NULL, device_name_buf);//创建 文件系统 的设备节点; 应用程序 通过文件系统的设备 节点 访问 硬件  
+static int spidev_probe(struct spi_device *spi)
+{
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+	struct spidev_data	*spidev;
+	int			status;
+	unsigned long		minor;
 
 
+	/* Allocate driver data */
+	spidev = kzalloc(sizeof(*spidev), GFP_KERNEL);
+
+	/* Initialize the driver data */
+	spidev->spi = spi;
+
+
+    /* 1 记录 spi dev */
+    dac_spi = spi;
+
+    /* 2 注册字符设备 */
+    major  = register_chrdev(0, "spi_dev_dac", &spidev_fops);
+    spidev_class = class_create(THIS_MODULE, "spi_dev_dac");
+    device_create(spidev_class, NULL, MKDEV(major, 0), NULL, "spi_dev_dac");
     
-    // major = register_chrdev(0, DEVICE_NAME, &my_test_device_fops);
-    // if (major < 0) {
-    //     printk(KERN_ALERT "Failed to register character device.\n");
-    //     return major;
-    // }
+    /* 3 */
 
-    // // 创建设备类
-    // // 为 THIS_MODULE  模块创建一个类
-    // my_test_device_class = class_create(THIS_MODULE, "my_test_device_class");
-    // if(IS_ERR(my_test_device_class)){
-    //     // pr_err("failed to allocate class\n");
-    //     printk("failed to allocate class\n");
-    //     return PTR_ERR(my_test_device_class);
-    // }
-    // /**
-    //  * 创建设备节点
-    //  *  params:
-    //  *      1. class: 设备类
-    //  *      2. dev_t: 设备号
-    //  *      3. const char *: 设备节点名称
-    //  *      4. dev_t: 设备号            MKDEV(major, 0) 是个整数
-    //  *      5. const char *: 设备节点名称
-    //  *    *      6. void *: 私有数据
-    //  * 
-    //  *  会去 创建一个 设备节点 /dev/my_test_device
-    //  */
-    // device_create(my_test_device_class, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
-    // printk(KERN_INFO "my_test_device registered with major number %d.\n", major);
-
-
-
-    return 0;
+	return status;
 }
 
-// 模块卸载时执行的函数
-static void __exit my_test_device_exit(void) {
+static void spidev_remove(struct spi_device *spi)
+{
+	struct spidev_data	*spidev = spi_get_drvdata(spi);
 
-    // 打印 my_buffer
-    printk(KERN_INFO "my_buffer: %s\n", my_buffer);
-
-    // 销毁类下面的 设备节点
-    // device_destroy(my_test_device_class, MKDEV(major, 0));
-    device_destroy(my_test_device_class, dev);
-    // 销毁类
-    class_destroy(my_test_device_class);
-
-    
-    // unregister_chrdev(major, DEVICE_NAME);
-    // 删除 cdev
-    cdev_del(&my_test_device_cdev);
-    // 释放设备号 释放 malloc 的区域
-    unregister_chrdev_region(dev, 2);
+	/* 1 注销字符设备 */
+    device_destroy(spidev_class, MKDEV(major, 0));
+    class_destroy(spidev_class);
+	unregister_chrdev(major, "spi_dev_dac");
+    /* 2 释放 spi dev */
 
 
-
-    printk(KERN_INFO "my_test_device unregistered.\n");
 }
 
-// 定义模块的加载和卸载函数
-module_init(my_test_device_init);
-module_exit(my_test_device_exit);
+static struct spi_driver spidev_spi_driver = {
+	.driver = {
+		.name =		"spidev,spi_dev_dac",
+		.of_match_table = spidev_dt_ids,
+	},
+	.probe =	spidev_probe,
+	.remove =	spidev_remove,
+	// .id_table =	spidev_spi_ids,
+
+	/* NOTE:  suspend/resume methods are not necessary here.
+	 * We don't do anything except pass the requests to/from
+	 * the underlying controller.  The refrigerator handles
+	 * most issues; the controller driver handles the rest.
+	 */
+};
+
+/*-------------------------------------------------------------------------*/
+
+static int __init spidev_init(void)
+{
+	int status;
+
+	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+	status = spi_register_driver(&spidev_spi_driver);
+	if (status < 0) {
+
+	}
+	return status;
+}
+module_init(spidev_init);
+
+static void __exit spidev_exit(void)
+{
+	printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+	spi_unregister_driver(&spidev_spi_driver);
+
+}
+module_exit(spidev_exit);
+
 
 MODULE_LICENSE("GPL");
+MODULE_ALIAS("spi:spidev");
--- resource/SPI/5 spi dev dac.md
+++ resource/SPI/5 spi dev dac.md
@@ -49,3 +49,10 @@ make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- dtbs -j$(nproc)
 dtc -I fs /sys/firmware/devicetree/base | less
 # 扩展
 
+
+
+
+
+sources/linux-rpi-6.6.y/drivers/spi/spidev.c
+
+
