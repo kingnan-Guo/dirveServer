086714d7adfd34309631da6e1397eab35260517a
kingnan-Guo <373734199@qq.com>
2025-10-19 15:55:01 +0000

    pwm

---
+++ modules/app_pwm/app_pwm.c
@@ -0,0 +1,15 @@
+#include "app_pwm.h"
+
+
+
+
+
+void app_pwm_init(int argc, char *argv[]){
+    printf("app_pwm_init\n");
+}
+
+
+
+void app_pwm_main(int argc, char *argv[]){
+    app_pwm_init(argc, argv);
+}
