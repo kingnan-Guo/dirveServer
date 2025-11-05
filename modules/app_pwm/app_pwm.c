2b42f267ed207ab368af11f706f1cb11719a863c
kingnan-Guo <373734199@qq.com>
2025-11-05 15:46:26 +0000

    更新呼吸灯功能

--- modules/app_pwm/app_pwm.c
+++ modules/app_pwm/app_pwm.c
@@ -18,7 +18,11 @@ int app_pwm_init(int argc, char *argv[]){
     printf("app_pwm_init ok\n");
 
     char buffer[10];
-    snprintf(buffer, sizeof(buffer), "%d", atoi(argv[2]));
+
+    int start_duty = 0;
+    int end_duty = 100;
+    start_duty = atoi(argv[2]);
+    snprintf(buffer, sizeof(buffer), "%d", start_duty);
 
 
     if(write(fd, buffer, strlen(buffer)) < 0){
@@ -26,6 +30,36 @@ int app_pwm_init(int argc, char *argv[]){
         return -1;
     }
     // close(fd);
+    // 呼吸灯
+
+
+    
+    int duty = start_duty;
+    int dir = 1;  // 1=上升, -1=下降
+
+    while (1) {
+        snprintf(buffer, sizeof(buffer), "%d", duty);
+        if (write(fd, buffer, strlen(buffer)) < 0) {
+            perror("write failed");
+            break;
+        }
+
+        // 更新占空比
+        duty += dir;
+        if (duty >= 100) {
+            duty = 100;
+            dir = -1; // 开始下降
+        } else if (duty <= 0) {
+            duty = 0;
+            dir = 1; // 开始上升
+        }
+
+        usleep(20000); // 每 20ms 更新一次亮度 ≈ 50Hz 呼吸灯节奏
+    }
+
+    close(fd);
+
+
 
     return 0;
 }
